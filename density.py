4e59fdeee22f27325ce7445c16f700e45657a325
oroafrica <52736386+oroafrica@users.noreply.github.com>
2019-11-10 13:30:16 +0200

    #1 density-init
    Calculates alloy weight based on volume based on republican cast and print

---
+++ density.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Moegamat Toffar - OROAFRICA 
+# ORO-TOOLS v1.0.0 2019
+
+import rhinoscriptsyntax as rs
+import Rhino as rhino
+
+def alloyWeight():
+    #alloy density
+    density = {"Sterling Silver":0.0105, "18K White":0.0176, "18K Yellow":0.0155,"9K White":0.014, "9K Yellow":0.012, "Platinum":0.0215}
+    
+    #create option list
+    alloy = []
+    for y in density:
+        alloy.append(y)
+        
+    #select object
+    objs = rs.GetObject("\nSelect PolySurfaces",rs.filter.polysurface)
+    #check for naked edges
+    if not rs.IsObjectSolid(objs):
+        rs.ClearCommandHistory() 
+        print("\nPolySurface not solid")
+        return
+        
+    #check for valid selection
+    if objs:
+        choice = rs.ListBox(alloy,"Select Alloy","Alloys")
+        wgt = round(rs.SurfaceVolume(objs)[0] * density.get(choice),2) 
+        rs.ClearCommandHistory() 
+        print("\nVolume: " + str(round(rs.SurfaceVolume(objs)[0],2)) + " amounts to " + str(wgt) + "g of " + choice) 
+    else:
+        rs.ClearCommandHistory()
+        print("\nNo valid PolySurface Selected!")   
+    
+if __name__ == "__main__":
+    alloyWeight()
